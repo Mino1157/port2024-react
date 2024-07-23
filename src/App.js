efb89075c719947fe83fd9241209ff10c3387beb
안민호 <wrs1030@naver.com>
2024-07-24 01:50:32 +0900

    마무리 완료

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React from "react";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import HomeView from "./views/HomeView";
+import lenis from "./utils/lenis";
+import link from "./utils/link";
 
+const App = () => {
+    useEffect(() => {
+        lenis();
+        link();
+    }, []);
 
-const App =  () => {
-    return(
-        <BrowserRouter>
+    return (
+    <BrowserRouter>
         <Routes>
-            <Route path="/" element={<HomeView />}/>
-            
+            <Route path="/" element={<HomeView />} />
         </Routes>
-        </BrowserRouter>
-    )
-}
+    </BrowserRouter>
+    );
+};
 
 export default App;
